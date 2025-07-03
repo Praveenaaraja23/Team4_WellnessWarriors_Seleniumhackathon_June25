4a75ef28a6ad6e45d82e6801702910c19ae8c06f
Lakshmi Priya Venkatesh <venkateshlakshmipriya@gmail.com>
2025-07-03 14:23:58 -0400

    Update ConfigReader.java

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -74,68 +74,7 @@ public class ConfigReader {
         else
             throw new RuntimeException("password not specified in the Configuration.properties file.");
     }
- /*   public static String getinvalidemail() {
-        String password = properties.getProperty("invalid");
-        if (password != null)
-            return password;
-        else
-            throw new RuntimeException("invalid not specified in the Configuration.properties file.");
-    } */
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-/*	static ThreadLocal<String> threadlocalbrowsertype = new ThreadLocal<>();
-	private static Properties properties;
-	private final static String propertyFilePath = "src/test/resources/dsAlgo_Configurations/config.properties";
-
-	// To get the path of config properties file
-	public static void loadConfig() {
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(propertyFilePath));
-			properties = new Properties();
-			try {
-				properties.load(reader);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			throw new RuntimeException("config.properties not found at " + propertyFilePath);
-		}
-
-	}
-
-	// Get the url details from config properties file
-
-	public static String getConfig(String key) {
-		String value = properties.getProperty(key);
-		if (value != null)
-			return value;
-		else
-			throw new RuntimeException("Key '" + key + "' not found in config.properties file.");
-	}
-
-	public static void setBrowserType(String browser) {
-		threadlocalbrowsertype.set(browser);
-	}
-
-	public static String getBrowserType() throws Throwable {
-
-		if (threadlocalbrowsertype.get() != null) {
-			return threadlocalbrowsertype.get();
-		} else {
-
-			throw new RuntimeException("browser is not specified in the testng.xml");
-		}
-	} */
 }
 
 
